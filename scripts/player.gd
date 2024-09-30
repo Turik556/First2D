441a8711d4be32aa6f87aba49e2107b03e69743c
turik556 <tar.ucheba@gmail.com>
2024-09-30 10:41:42 +0300

    new

--- scripts/player.gd
+++ scripts/player.gd
@@ -47,10 +47,9 @@ func _process(delta: float) -> void:
 		#$AnimatedSprite2D.flip_v = velocity.y > 0
 	pass
 
-
-
 func _on_body_entered(body: Node2D) -> void:
 	 # Player disappears after being hit.
+
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
