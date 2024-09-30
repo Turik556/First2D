d83c36c622873c18105808415de27501dd159b4a
turik556 <tar.ucheba@gmail.com>
2024-09-30 12:28:53 +0300

    test classes

--- scripts/item.gd
+++ scripts/item.gd
@@ -4,10 +4,16 @@ class_name Item
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.s_game_over.connect(remove)
+	function()
 	#тут будет решаться рандомом какой предмет будет спавниться
 	
 func _process(delta: float) -> void:
 	pass
+
+func function():
+	print("parent")
+	pass
+
 func remove():
 	queue_free()
 	pass
