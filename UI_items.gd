2c4befe0c9024b2f7edcf231a6022abb206c99a6
turik556 <tar.ucheba@gmail.com>
2024-10-23 14:56:42 +0300

    add element to dict

--- UI_items.gd
+++ UI_items.gd
@@ -3,9 +3,10 @@ extends HBoxContainer
 class_name UI_item
 var icons_dict = { 
 	"froze": preload("res://art/items/snowflake.png"),
-	"speed_up" : "str ",
-	"shield" : preload("res://art/items/icons/shield.png")
-	
+	"speed_up" : preload("res://art/items/icons/skull_01b.png"),
+	"shield" : preload("res://art/items/icons/shield.png"),
+	"smaller" : preload("res://art/items/icons/potion_03b.png"),
+	"bigger" : preload("res://art/items/icons/potion_03a.png")
 }
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +15,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func set_texture():
+	
 	pass
